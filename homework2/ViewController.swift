9b402ad09c424cb819e27201701f9ed2331690e9
marso <marso.yu777@gmail.com>
2020-11-20 01:26:08 +0800

    Add Random Color Button

--- homework2/ViewController.swift
+++ homework2/ViewController.swift
@@ -26,17 +26,36 @@ class ViewController: UIViewController {
     @IBOutlet weak var blueSliderLabel: UILabel!
     @IBOutlet weak var alPhaSliderLabel: UILabel!
     
-    
-    
-    @IBAction func backgroundColor(_ sender: UISlider) {
-        machikoImage.backgroundColor = UIColor(red: CGFloat(redSlider.value/255), green: CGFloat(greenSlider.value/255), blue: CGFloat(blueSlider.value/255), alpha: CGFloat(alphaSlider.value))
+    @IBAction func RandomColor(_ sender: Any) {
+        let redRandom = Float.random(in: 0...255)
+        let greenRandom = Float.random(in: 0...255)
+        let blueRandom = Float.random(in: 0...255)
+        let alphaRandom = Float.random(in: 0...1)
         
-        redSliderLabel.text = String(format: "Red : %.2f", redSlider.value)
-        greenSliderLabel.text = String(format: "Greem : %.2f", greenSlider.value)
-        blueSliderLabel.text = String(format: "Blue : %.2f", blueSlider.value)
-        alPhaSliderLabel.text = String(format: "Alpha : %.2f", alphaSlider.value)
+        backgroundColorSetting(redRandom, greenRandom, blueRandom, alphaRandom)
     }
     
+    
+    @IBAction func backgroundColorControlBySlider(_ sender: UISlider) {
+        backgroundColorSetting(redSlider.value, greenSlider.value, blueSlider.value, alphaSlider.value)
+    }
+    
+    func backgroundColorSetting(_ red:Float, _ green:Float, _ blue:Float, _ alpha:Float){
+        
+        redSlider.value = red
+        greenSlider.value = green
+        blueSlider.value = blue
+        alphaSlider.value = alpha
+        
+        machikoImage.backgroundColor = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: CGFloat(alphaSlider.value))
+        
+        redSliderLabel.text = String(format: "Red : %.2f", red)
+        greenSliderLabel.text = String(format: "Green : %.2f", green)
+        blueSliderLabel.text = String(format: "Blue : %.2f", blue)
+        alPhaSliderLabel.text = String(format: "Alpha : %.2f", alpha)
+        
+        
+    }
     
 
 
