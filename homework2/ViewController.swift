e1ffa3e4dc5fc125738582863e9e57eab0b7c529
Marso <marso.yu777@gmail.com>
2020-11-21 16:32:13 +0800

    Switch 待研究

--- homework2/ViewController.swift
+++ homework2/ViewController.swift
@@ -7,36 +7,92 @@
 
 import UIKit
 
+let gradientLayer = CAGradientLayer()
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         machikoImage.image = UIImage(named: "machiko")
-        //machikoImage.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        backgroundColorSetting(255, 255, 255, 1)
+        gradientSlider.isEnabled = false
 
     }
+    @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var machikoImage: UIImageView!
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var gradientSlider: UISlider!
     @IBOutlet weak var alphaSlider: UISlider!
     @IBOutlet weak var redSliderLabel: UILabel!
     @IBOutlet weak var greenSliderLabel: UILabel!
     @IBOutlet weak var blueSliderLabel: UILabel!
     @IBOutlet weak var alPhaSliderLabel: UILabel!
+    @IBOutlet weak var gradientSwitch: UISwitch!
+    @IBOutlet weak var RandomColorButton: UIButton!
     
-    @IBAction func RandomColor(_ sender: Any) {
+    
+    @IBAction func gradientMode(_ sender: Any) {
+        if gradientSwitch.isOn{
+            machikoImage.image = UIImage(named: "machikoLine")
+            backgroundColorSetting(0, 0, 0, 0)
+            redSlider.isEnabled = false
+            greenSlider.isEnabled = false
+            blueSlider.isEnabled = false
+            alphaSlider.isEnabled = false
+            RandomColorButton.isEnabled = false
+            gradientSlider.isEnabled = true
+        }
+        else {
+            machikoImage.image = UIImage(named: "machiko")
+            backgroundColorSetting(255, 255, 255, 1)
+            redSlider.isEnabled = true
+            greenSlider.isEnabled = true
+            blueSlider.isEnabled = true
+            alphaSlider.isEnabled = true
+            RandomColorButton.isEnabled = true
+            gradientSlider.isEnabled = false
+            backgroundView.layer.sublayers?.removeAll()
+            //backgroundView.layer.addSublayer(machikoImage.layer)
+        }
+        
+    }
+
+    @IBAction func backgroundClearButton(_ sender: Any) {
+        backgroundColorSetting(255, 255, 255, 1)
+    }
+    
+    @IBAction func backgroundGradientSlider(_ sender: UISlider) {
+        gradientLayer.frame = backgroundView.bounds
+        gradientLayer.colors = [
+            UIColor.red.cgColor,
+            UIColor.orange.cgColor,
+            UIColor.yellow.cgColor,
+            UIColor.green.cgColor,
+            UIColor.blue.cgColor,
+            UIColor.purple.cgColor
+        ]
+        
+        gradientLayer.locations = [
+            0,
+            NSNumber(value:gradientSlider.value*1),
+            NSNumber(value:gradientSlider.value*2),
+            NSNumber(value:gradientSlider.value*3),
+            NSNumber(value:gradientSlider.value*4)
+        ]
+        backgroundView.mask = machikoImage
+        backgroundView.layer.addSublayer(gradientLayer)
+    }
+    
+    @IBAction func RandomColorButton(_ sender: Any) {
         let redRandom = Float.random(in: 0...255)
         let greenRandom = Float.random(in: 0...255)
         let blueRandom = Float.random(in: 0...255)
         let alphaRandom = Float.random(in: 0...1)
-        
         backgroundColorSetting(redRandom, greenRandom, blueRandom, alphaRandom)
     }
     
-    
-    @IBAction func backgroundColorControlBySlider(_ sender: UISlider) {
+    @IBAction func backgroundColorControlSlider(_ sender: UISlider) {
         backgroundColorSetting(redSlider.value, greenSlider.value, blueSlider.value, alphaSlider.value)
     }
     
@@ -53,8 +109,10 @@ class ViewController: UIViewController {
         greenSliderLabel.text = String(format: "Green : %.2f", green)
         blueSliderLabel.text = String(format: "Blue : %.2f", blue)
         alPhaSliderLabel.text = String(format: "Alpha : %.2f", alpha)
-        
-        
+    }
+    
+    func defaultColor(){
+        machikoImage.backgroundColor = UIColor(red: CGFloat(1), green: CGFloat(1), blue: CGFloat(1), alpha: CGFloat(1))
     }
     
 
