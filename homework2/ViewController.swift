168ceb988d3e7e20d857e523b079ae9eda2ddc36
Marso <marso.yu777@gmail.com>
2020-11-23 11:13:34 +0800

    1. 修正用Storyboard ImageView來做Mask的錯誤方法, 應從程產生ImageView即可 2. 完成Gradient Mask 及 UI 顯示介面。 3. Edit App Display Name 4. Add App Icon

--- homework2/ViewController.swift
+++ homework2/ViewController.swift
@@ -7,63 +7,115 @@
 
 import UIKit
 
-let gradientLayer = CAGradientLayer()
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        machikoImage.image = UIImage(named: "machiko")
-        backgroundColorSetting(255, 255, 255, 1)
+        self.view.backgroundColor = UIColor(patternImage: UIImage(imageLiteralResourceName: "machikoBackground"))
+        ShowImageView.image = UIImage(named: "machiko")
+        //backgroundColorSetting(255, 255, 255, 1)
         gradientSlider.isEnabled = false
 
     }
-    @IBOutlet weak var backgroundView: UIView!
-    @IBOutlet weak var machikoImage: UIImageView!
+    
+    let gradientLayer = CAGradientLayer()
+    @IBOutlet weak var ShowUIView: UIView!
+    @IBOutlet weak var ShowImageView: UIImageView!
+    
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     @IBOutlet weak var gradientSlider: UISlider!
     @IBOutlet weak var alphaSlider: UISlider!
+    
     @IBOutlet weak var redSliderLabel: UILabel!
     @IBOutlet weak var greenSliderLabel: UILabel!
     @IBOutlet weak var blueSliderLabel: UILabel!
     @IBOutlet weak var alPhaSliderLabel: UILabel!
+    @IBOutlet weak var gradientSliderLabel: UILabel!
+    @IBOutlet weak var ModeLabel: UILabel!
+    
     @IBOutlet weak var gradientSwitch: UISwitch!
     @IBOutlet weak var RandomColorButton: UIButton!
+
     
     
     @IBAction func gradientMode(_ sender: Any) {
         if gradientSwitch.isOn{
-            machikoImage.image = UIImage(named: "machikoLine")
-            backgroundColorSetting(0, 0, 0, 0)
+            ModeLabel.text = "Gradient Mask"
+            drawMaskImage()
             redSlider.isEnabled = false
             greenSlider.isEnabled = false
             blueSlider.isEnabled = false
             alphaSlider.isEnabled = false
-            RandomColorButton.isEnabled = false
             gradientSlider.isEnabled = true
         }
         else {
-            machikoImage.image = UIImage(named: "machiko")
-            backgroundColorSetting(255, 255, 255, 1)
+            ModeLabel.text = "Backgroud Color"
+            ShowImageView.image = UIImage(named: "machiko")
             redSlider.isEnabled = true
             greenSlider.isEnabled = true
             blueSlider.isEnabled = true
             alphaSlider.isEnabled = true
-            RandomColorButton.isEnabled = true
             gradientSlider.isEnabled = false
-            backgroundView.layer.sublayers?.removeAll()
-            //backgroundView.layer.addSublayer(machikoImage.layer)
+            
+            //reset gradient & mask
+            gradientLayer.removeFromSuperlayer()
+            ShowUIView.mask = nil
         }
         
     }
 
     @IBAction func backgroundClearButton(_ sender: Any) {
-        backgroundColorSetting(255, 255, 255, 1)
+        if gradientSwitch.isOn{
+            gradientSlider.value = 0.2
+            drawMaskImage()
+        }
+        else{
+            backgroundColorSetting(255, 255, 255, 1)
+        }
     }
     
     @IBAction func backgroundGradientSlider(_ sender: UISlider) {
-        gradientLayer.frame = backgroundView.bounds
+        drawMaskImage()
+    }
+    
+    @IBAction func RandomColorButton(_ sender: Any) {
+        if gradientSwitch.isOn{
+            //let gradientRandom = Float.random(in: 0.0...1)
+            //gradientSlider.value = gradientRandom
+            gradientSlider.value = Float.random(in: 0.0...1)
+            drawMaskImage()
+        }
+        else{
+            let redRandom = Float.random(in: 0...255)
+            let greenRandom = Float.random(in: 0...255)
+            let blueRandom = Float.random(in: 0...255)
+            let alphaRandom = Float.random(in: 0...1)
+            backgroundColorSetting(redRandom, greenRandom, blueRandom, alphaRandom)
+        }
+    }
+    
+    @IBAction func backgroundColorControlSlider(_ sender: UISlider) {
+        backgroundColorSetting(redSlider.value, greenSlider.value, blueSlider.value, alphaSlider.value)
+    }
+    
+    func backgroundColorSetting(_ red:Float, _ green:Float, _ blue:Float, _ alpha:Float){
+        redSlider.value = red
+        greenSlider.value = green
+        blueSlider.value = blue
+        alphaSlider.value = alpha
+        
+        ShowImageView.backgroundColor = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: CGFloat(alphaSlider.value))
+        
+        redSliderLabel.text = String(format: "Red : %.2f", red)
+        greenSliderLabel.text = String(format: "Green : %.2f", green)
+        blueSliderLabel.text = String(format: "Blue : %.2f", blue)
+        alPhaSliderLabel.text = String(format: "Alpha : %.2f", alpha)
+    }
+    
+    func drawMaskImage(){
+        gradientLayer.frame = ShowUIView.bounds
         gradientLayer.colors = [
             UIColor.red.cgColor,
             UIColor.orange.cgColor,
@@ -80,43 +132,15 @@ class ViewController: UIViewController {
             NSNumber(value:gradientSlider.value*3),
             NSNumber(value:gradientSlider.value*4)
         ]
-        backgroundView.mask = machikoImage
-        backgroundView.layer.addSublayer(gradientLayer)
-    }
-    
-    @IBAction func RandomColorButton(_ sender: Any) {
-        let redRandom = Float.random(in: 0...255)
-        let greenRandom = Float.random(in: 0...255)
-        let blueRandom = Float.random(in: 0...255)
-        let alphaRandom = Float.random(in: 0...1)
-        backgroundColorSetting(redRandom, greenRandom, blueRandom, alphaRandom)
-    }
-    
-    @IBAction func backgroundColorControlSlider(_ sender: UISlider) {
-        backgroundColorSetting(redSlider.value, greenSlider.value, blueSlider.value, alphaSlider.value)
-    }
-    
-    func backgroundColorSetting(_ red:Float, _ green:Float, _ blue:Float, _ alpha:Float){
         
-        redSlider.value = red
-        greenSlider.value = green
-        blueSlider.value = blue
-        alphaSlider.value = alpha
-        
-        machikoImage.backgroundColor = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: CGFloat(alphaSlider.value))
-        
-        redSliderLabel.text = String(format: "Red : %.2f", red)
-        greenSliderLabel.text = String(format: "Green : %.2f", green)
-        blueSliderLabel.text = String(format: "Blue : %.2f", blue)
-        alPhaSliderLabel.text = String(format: "Alpha : %.2f", alpha)
-    }
+        let machikoLineView = UIImage(named: "machikoLine")
+        let machikoLineImageview = UIImageView(image: machikoLineView)
+        machikoLineImageview.frame.size = CGSize(width: 350, height: 350)
+        ShowUIView.mask = machikoLineImageview
+        ShowUIView.layer.addSublayer(gradientLayer)
     
-    func defaultColor(){
-        machikoImage.backgroundColor = UIColor(red: CGFloat(1), green: CGFloat(1), blue: CGFloat(1), alpha: CGFloat(1))
+        gradientSliderLabel.text = String(format: "Gradient : %.2f", gradientSlider.value)
     }
-    
-
-
 
 }
 
